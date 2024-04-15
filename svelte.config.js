75a5203b734b086d759f4b15f347fbee1082fc94
Imagineee <59028351+imagineeeinc@users.noreply.github.com>
2024-04-15 15:45:21 +0100

    Update svelte.config.js for vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 /* const config = {
@@ -12,7 +12,7 @@
 
 export default config; */
 
-import adapter from '@sveltejs/adapter-node'
+// import adapter from '@sveltejs/adapter-node'
 
 export default {
   kit: {
