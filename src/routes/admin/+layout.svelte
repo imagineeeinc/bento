6c6465f4caac5d61cbf1934ca4008eaeeb32bc8b
imagineeeinc <59028351+imagineeeinc@users.noreply.github.com>
2024-04-17 13:31:52 +0100

    added admin panel files

---
+++ src/routes/admin/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import '../../app.css'
+</script>
+<svelte:head>
+    <title>Admin - Bento</title> 
+</svelte:head>
+
+<slot />
---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { browser } from '$app/environment'
+
+  if (browser) {
+  }
+</script>
+  <h2>Admin Panel</h2>
+  <h4>Configure settings on this instance</h4>
+<style>
+</style>
