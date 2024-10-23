540f474095283f9ea269cabc2a9b2fd4aaa51c6a
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 <59028351+imagineeeinc@users.noreply.github.com>
2024-10-23 11:33:00 +0100

    added title bar theme changing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,13 @@
 
   <meta name="color-scheme" content={$theme == 'system' ? 'light dark' : $theme}/>
   <link rel="stylesheet" href={`/theme/${$theme}.css`} />
+
+  {#if $theme == 'system'}
+    <meta name="theme-color" media="(prefers-color-scheme: light)" content="#f0f5f3" />
+    <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#0a0f0d" />
+  {:else}
+    <meta name="theme-color" content={$theme == 'dark' ? '#0a0f0d' : '#f0f5f3'} />
+  {/if}
 </svelte:head>
 
 <slot />
